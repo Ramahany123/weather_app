41afd845345756e39608c1902e28bb7d083206ab
Rama Hany <hanyrama703@gmai.com>
2024-12-21 16:08:57 +0200

    Added error handling  to WeatherService

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:weather_app/models/weather_model.dart';
 
@@ -7,10 +9,19 @@ class WeatherService {
   final String apiKey = "b7208b61350f4bca93e155814241912";
   WeatherService(this.dio);
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response =
-        await dio.get("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1");
+    try {
+      Response response = await dio
+          .get("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1");
 
-    WeatherModel weatherModel = response.data;
-    return weatherModel;
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMssg = e.response?.data["error"]["message"] ??
+          "oops there was an error, try later";
+      throw Exception(errorMssg);
+    } catch (e) {
+      log(e.toString());
+      throw Exception("oops there was an error, try later");
+    }
   }
 }
