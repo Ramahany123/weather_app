048a8d9580c266fc142dcf887653b9ae735623d7
Rama Hany <hanyrama703@gmai.com>
2025-01-26 10:55:16 +0200

    added theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
+import 'package:weather_app/methods/get_color_theme.dart';
 import 'package:weather_app/views/home_view.dart';
 
 void main() {
@@ -14,95 +16,41 @@ class WeatherApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GetWeatherCubit(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomeView(),
-      ),
+      child: const WeatherAppTheme(),
     );
   }
 }
 
-MaterialColor getColorTheme(String condition) {
-  // Group similar conditions and assign colors
-  switch (condition) {
-    // Sunny/Clear
-    case "Sunny":
-    case "Clear":
-      return Colors.orange;
+class WeatherAppTheme extends StatelessWidget {
+  const WeatherAppTheme({
+    super.key,
+  });
 
-    // Partly Cloudy
-    case "Partly cloudy":
-      return Colors.lightBlue;
-
-    // Cloudy/Overcast
-    case "Cloudy":
-    case "Overcast":
-      return Colors.blueGrey;
-
-    // Mist/Fog
-    case "Mist":
-    case "Fog":
-    case "Freezing fog":
-      return Colors.grey;
-
-    // Rainy Conditions
-    case "Patchy rain possible":
-    case "Patchy light drizzle":
-    case "Light drizzle":
-    case "Patchy light rain":
-    case "Light rain":
-    case "Moderate rain at times":
-    case "Moderate rain":
-    case "Heavy rain at times":
-    case "Heavy rain":
-    case "Light rain shower":
-    case "Moderate or heavy rain shower":
-    case "Torrential rain shower":
-      return Colors.blue;
-
-    // Snowy Conditions
-    case "Patchy snow possible":
-    case "Patchy light snow":
-    case "Light snow":
-    case "Patchy moderate snow":
-    case "Moderate snow":
-    case "Patchy heavy snow":
-    case "Heavy snow":
-    case "Light snow showers":
-    case "Moderate or heavy snow showers":
-      return Colors.lightBlue;
-
-    // Sleet/Freezing Rain
-    case "Patchy sleet possible":
-    case "Patchy freezing drizzle possible":
-    case "Freezing drizzle":
-    case "Heavy freezing drizzle":
-    case "Light freezing rain":
-    case "Moderate or heavy freezing rain":
-    case "Light sleet":
-    case "Moderate or heavy sleet":
-    case "Light sleet showers":
-    case "Moderate or heavy sleet showers":
-      return Colors.cyan;
-
-    // Thunderstorms
-    case "Thundery outbreaks possible":
-    case "Patchy light rain with thunder":
-    case "Moderate or heavy rain with thunder":
-    case "Patchy light snow with thunder":
-    case "Moderate or heavy snow with thunder":
-      return Colors.deepPurple;
-
-    // Extreme Conditions
-    case "Blowing snow":
-    case "Blizzard":
-    case "Ice pellets":
-    case "Light showers of ice pellets":
-    case "Moderate or heavy showers of ice pellets":
-      return Colors.indigo;
-
-    // Default color for unknown conditions
-    default:
-      return Colors.grey;
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetWeatherCubit, WeatherState>(
+      builder: (context, state) {
+        String? weatherCondition = BlocProvider.of<GetWeatherCubit>(context)
+            .weatherModel
+            ?.weatherCondition;
+        // To Do: write theme in a cleaner way
+        ThemeData theme = ThemeData(
+          primarySwatch: getColorTheme(weatherCondition),
+          appBarTheme: AppBarTheme(
+            color: getColorTheme(weatherCondition),
+            titleTextStyle: const TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        );
+        return MaterialApp(
+          theme: theme,
+          debugShowCheckedModeBanner: false,
+          home: const HomeView(),
+        );
+      },
+    );
   }
 }
